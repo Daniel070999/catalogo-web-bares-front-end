ae95f24823159714f1e409a620e0ee7467ca0420
Daniel070999 <danielixo99@gmail.com>
2023-07-17 22:45:30 -0500

    interfaz registro

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -6,5 +6,35 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
+  //variables de login
+  loginUsername: string = "";
+  loginPassword: string = "";
+  //variables de registro
+  registerName: string = "";
+  registerLastName: string = "";
+  registerUser: string = "";
+  registerEmail: string = "";
+  registerGenero: string = "";
+  registerDateBirth: string = "";
+  registerPhone: string = "";
+  registerPassword: string = "";
+  registerPasswordValidate: string = "";
 
+  login() {
+    // Aquí puedes agregar la lógica para autenticar al usuario
+    console.log('Iniciando sesión...');
+  }
+
+  register() {
+    // Aquí puedes agregar la lógica para registrar al usuario
+    console.log(this.registerName + '\n'
+      + this.registerLastName + '\n'
+      + this.registerUser + '\n'
+      + this.registerEmail + '\n'
+      + this.registerGenero + '\n'
+      + this.registerDateBirth + '\n'
+      + this.registerPhone + '\n'
+      + this.registerPassword + '\n'
+      + this.registerPasswordValidate + '\n');
+  }
 }
